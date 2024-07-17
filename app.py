051e6ff2912494b198afd42d61d5fc2ee703a666
Amol Gupta <62533644+FighterX777@users.noreply.github.com>
2024-07-18 00:13:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -87,5 +87,4 @@ def predict():
         return render_template(output_page, pred_output = pred, user_image = file_path)
 
 
-if __name__ == "__main__":
-    app.run(debug=True,threaded=False,host='0.0.0.0')
+
